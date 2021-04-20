7edccd8d9ceeff9af9421f38efe821a738034ed3
adinugrahadharmaputra <adinugrahadharmaputra@gmail.com>
2021-04-20 14:01:39 +0700

    setup model and admin

--- destinasiWisata/destinasi/admin.py
+++ destinasiWisata/destinasi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from destinasi.models import Destinasi,Provinsi
 
 # Register your models here.
+admin.site.register(Destinasi)
+admin.site.register(Provinsi)
+
--- destinasiWisata/destinasi/models.py
+++ destinasiWisata/destinasi/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Provinsi(models.Model):
+    nama = models.CharField(max_length=40)
+    deskripsi = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
+
+class Destinasi(models.Model):
+    nama = models.CharField(max_length=50)
+    deskripsi = models.TextField()
+    gambar_url = models.URLField()
+    alamat = models.TextField()
+    situs = models.URLField()
+    telepon = models.CharField(max_length=15,null=True)
+    provinsi_id = models.ForeignKey(Provinsi, on_delete=models.CASCADE, null=True)
+    
+
+    def __str__(self):
+        return self.nama
